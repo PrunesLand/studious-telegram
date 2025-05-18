6ec5cc01e585fc9e75b5d66ff7e6591eeaa43b55
PrunesLand <panargya@gmail.com>
2025-05-18 18:45:19 +0200

    Create new dashboard screen

---
+++ lib/features/dashboard/presentation/screen/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: Text('Your history is empty.'))
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon:Icon(Icons.list), label: 'History'),
+        BottomNavigationBarItem(icon:Icon(Icons.settings), label: 'Settings'),
+        ]),
+      floatingActionButton: FloatingActionButton(onPressed: () => {}, child: Icon(Icons.add),),
+    );
+  }
+}
